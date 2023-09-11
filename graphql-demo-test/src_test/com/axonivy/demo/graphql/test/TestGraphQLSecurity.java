1917718ed04ecee9a614a91d58bbcd9e30366ebb
Louis <louis.mueller@axonivy.com>
2023-09-11 16:32:57 +0200

    add tests

---
+++ graphql-demo-test/src_test/com/axonivy/demo/graphql/test/TestGraphQLSecurity.java
@@ -0,0 +1,60 @@
+package com.axonivy.demo.graphql.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import org.junit.jupiter.api.Test;
+import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.environment.IvyTest;
+import ch.ivyteam.ivy.security.ISecurity;
+import ch.ivyteam.ivy.security.user.IUserRepository;
+import ch.ivyteam.ivy.security.user.NewUser;
+
+@IvyTest
+class TestGraphQLSecurity {
+
+  WebTarget graphQlClient = Ivy.rest().client("test-graphql");
+  IUserRepository userRepo = ISecurity.current().users();
+
+  @Test
+  void allUsers() {
+    assertThat(userRepo.count()).isZero();
+    var user = userRepo.create(NewUser.create("testUser").toNewUser());
+    var requestEntity = requestEntity("{allUsers {id name isEnabled}}");
+    var expectedMessage = """
+            {"data":{"allUsers":[{"id":"%s","name":"testUser","isEnabled":true}]}}"""
+            .formatted(user.getSecurityMemberId());
+    requestAndAssert(requestEntity, expectedMessage);
+    userRepo.delete("testUser");
+  }
+
+  @Test
+  void createUser() {
+    assertThat(userRepo.count()).isZero();
+    var query = "mutation{createUser(userName: \\\"createdUser\\\", password: \\\"aPassword\\\") {name isEnabled}}";
+    var requestEntity = requestEntity(query);
+    var expectedMessage = """
+            {"data":{"createUser":{"name":"createdUser","isEnabled":true}}}""";
+    requestAndAssert(requestEntity, expectedMessage);
+    assertThat(userRepo.count()).isOne();
+    userRepo.delete("createdUser");
+  }
+
+  String requestEntity(String query) {
+    return """
+            {
+              "variables": {},
+              "query": "%s"
+            }
+            """.formatted(query);
+  }
+
+  void requestAndAssert(String requestEntity, String expectedMessage) {
+    var response = graphQlClient.request()
+            .header("X-Requested-By", "ivy")
+            .post(Entity.json(requestEntity));
+    assertThat(response.getStatus()).isEqualTo(200);
+    var message = response.readEntity(String.class);
+    assertThat(message).isEqualTo(expectedMessage);
+  }
+}
---
+++ graphql-demo-test/src_test/com/axonivy/demo/graphql/test/TestGraphQLWorkflow.java
@@ -0,0 +1,82 @@
+package com.axonivy.demo.graphql.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.environment.IvyTest;
+import ch.ivyteam.ivy.workflow.ITask;
+
+@IvyTest
+class TestGraphQLWorkflow {
+
+  WebTarget graphQlClient = Ivy.rest().client("test-graphql");
+
+  @BeforeEach
+  @SuppressWarnings("restriction")
+  void setup() {
+    ((ch.ivyteam.ivy.workflow.internal.Task) ITask.current()).makePersistent();
+  }
+
+  @Test
+  void allTasks() {
+    var requestEntity = requestEntity("{allTasks {id name state}}");
+    var expectedMessage = """
+            {"data":{"allTasks":[{"id":"1","name":"test process","state":"CREATED"}]}}""";
+    requestAndAssert(requestEntity, expectedMessage);
+  }
+
+  @Test
+  void taskById() {
+    var requestEntity = requestEntity("{taskById(id: 1) {priority state}}");
+    var expectedMessage = """
+            {"data":{"taskById":{"priority":"NORMAL","state":"CREATED"}}}""";
+    requestAndAssert(requestEntity, expectedMessage);
+  }
+
+  @Test
+  void taskByIdNoMatch() {
+    var requestEntity = requestEntity("{taskById(id: 999) {id name state}}");
+    var expectedMessage = """
+            {"data":{"taskById":null}}""";
+    requestAndAssert(requestEntity, expectedMessage);
+  }
+
+  @Test
+  void tasksByState() {
+    var query = "{tasksByState(state: \\\"CREATED\\\") {priority state}}";
+    var requestEntity = requestEntity(query);
+    var expectedMessage = """
+            {"data":{"tasksByState":[{"priority":"NORMAL","state":"CREATED"}]}}""";
+    requestAndAssert(requestEntity, expectedMessage);
+  }
+
+  @Test
+  void tasksByStateNoMatch() {
+    var query = "{tasksByState(state: \\\"DONE\\\") {id name}}";
+    var requestEntity = requestEntity(query);
+    var expectedMessage = """
+            {"data":{"tasksByState":[]}}""";
+    requestAndAssert(requestEntity, expectedMessage);
+  }
+
+  String requestEntity(String query) {
+    return """
+            {
+              "variables": {},
+              "query": "%s"
+            }
+            """.formatted(query);
+  }
+
+  void requestAndAssert(String requestEntity, String expectedMessage) {
+    var response = graphQlClient.request()
+            .header("X-Requested-By", "ivy")
+            .post(Entity.json(requestEntity));
+    assertThat(response.getStatus()).isEqualTo(200);
+    var message = response.readEntity(String.class);
+    assertThat(message).isEqualTo(expectedMessage);
+  }
+}
